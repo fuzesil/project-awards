a1a8be3c989bb337159299f700f9e77cf8b72fcf
fuzesil <106630513+fuzesil@users.noreply.github.com>
2026-05-25 20:30:09 +0200

    MAJOR REEFACTORING of Tests

--- Source/Awards.Test/InternalTesting.cs
+++ Source/Awards.Test/InternalTesting.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.Metrics;
     using System.Linq;
     using Awards.Data;
     using Awards.Logic;
@@ -15,67 +16,192 @@
     [TestFixture]
     public class InternalTesting
     {
+        private InternalAuditLogic internalLogic;
+
+        private Mock<ICountryRepository> countryRepo;
+        private Mock<IMemberRepository> memberRepo;
+        private Mock<IExpertGroupRepository> egRepo;
+
+        private IList<ExpertGroup> egList;
+        private IList<Country> countryList;
+        private IList<Member> memberList;
+
+        /// <summary>
+        /// The method to be called immediately before each <see cref="InternalTesting"/> test is run.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.countryRepo = new Mock<ICountryRepository>(MockBehavior.Loose);
+            this.egRepo = new Mock<IExpertGroupRepository>(MockBehavior.Loose);
+            this.memberRepo = new Mock<IMemberRepository>(MockBehavior.Loose);
+            this.internalLogic = new InternalAuditLogic(2, this.memberRepo.Object, this.countryRepo.Object, this.egRepo.Object);
+            this.countryList = MockRepoGenerator.GenerateCountryList();
+            this.egList = MockRepoGenerator.GenerateExpertGroupList();
+            this.memberList = MockRepoGenerator.GenerateMemberList();
+        }
+
+        /// <summary>
+        /// Testing the return value of <see cref="InternalLogic.GetOneCountry(int, string)"/>.
+        /// </summary>
+        /// <param name="id">Any integer.</param>
+        /// <param name="name">Any of the names in the data set.</param>
+        [TestCase(1, "Australia")]
+        [TestCase(7, "Hungary")]
+        [TestCase(int.MaxValue / 2, "qwertzuiop")]
+        [TestCase(int.MinValue, "Invalid")]
+        [TestCase(-1, null)]
+        [TestCase(0, "")]
+        public void TestGetOneCountryReturnsValidOrNull(int id, string name)
+        {
+            this.countryRepo.Setup(repo => repo.GetOne(id))
+                .Returns<int>(idArg => this.countryList.SingleOrDefault(c => c.CountryID == idArg));
+            this.countryRepo.Setup(repo => repo.GetOne(name))
+                .Returns<string>(nameArg => this.countryList.FirstOrDefault(c => c.Name == nameArg));
+
+            Country expectedCountryById = this.internalLogic.GetOneCountry(id);
+            Country expectedCountryByName = this.internalLogic.GetOneCountry(It.Is<int>(i => i < 1), name);
+
+            Assert.AreEqual(expectedCountryById, this.countryList.SingleOrDefault(c => c.CountryID == id));
+            Assert.AreEqual(expectedCountryByName, this.countryList.FirstOrDefault(c => c.Name == name));
+            Assert.AreEqual(expectedCountryById, expectedCountryByName);
+            this.countryRepo.Verify(repo => repo.GetOne(id), Times.AtMostOnce);
+            this.countryRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.AtMostOnce);
+            this.countryRepo.Verify(repo => repo.GetOne(name), Times.AtMostOnce);
+            this.countryRepo.Verify(repo => repo.GetOne(It.IsAny<string>()), Times.AtMostOnce);
+            this.countryRepo.Verify(repo => repo.GetAll(), Times.Never);
+        }
+
+        /// <summary>
+        /// Testing the return value of <see cref="InternalLogic.GetOneExpertGroup(int, string)"/>.
+        /// </summary>
+        /// <param name="id">Any integer.</param>
+        /// <param name="name">Any of the names in the data set.</param>
+        [TestCase(1, "Hi-Fi")]
+        [TestCase(3, "Home Theatre Display & Video")]
+        [TestCase(int.MaxValue / 2, "qwertzuiop")]
+        [TestCase(int.MinValue, "Invalid")]
+        [TestCase(-1, null)]
+        [TestCase(0, "")]
+        public void TestGetOneExpertgroupReturnsValidOrNull(int id, string name)
+        {
+            this.egRepo.Setup(repo => repo.GetOne(id))
+                .Returns<int>(idArg => this.egList.SingleOrDefault(eg => eg.ExpertGroupID == idArg));
+            this.egRepo.Setup(repo => repo.GetOne(name))
+                .Returns<string>(nameArg => this.egList.FirstOrDefault(eg => eg.Name == nameArg));
+
+            ExpertGroup resultEGById = this.internalLogic.GetOneExpertGroup(id);
+            ExpertGroup resultEGByName = this.internalLogic.GetOneExpertGroup(It.Is<int>(i => i < 1), name);
+
+            Assert.AreEqual(resultEGById, this.egList.SingleOrDefault(eg => eg.ExpertGroupID == id));
+            Assert.AreEqual(resultEGByName, this.egList.FirstOrDefault(eg => eg.Name == name));
+            Assert.AreEqual(resultEGById, resultEGByName);
+            this.egRepo.Verify(repo => repo.GetOne(id), Times.AtMostOnce);
+            this.egRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.AtMostOnce);
+            this.egRepo.Verify(repo => repo.GetOne(name), Times.AtMostOnce);
+            this.egRepo.Verify(repo => repo.GetOne(It.IsAny<string>()), Times.AtMostOnce);
+            this.egRepo.Verify(repo => repo.GetAll(), Times.Never);
+        }
+
+        /// <summary>
+        /// Testing the return value of <see cref="InternalLogic.GetOneMember(int, string)"/> method.
+        /// </summary>
+        /// <param name="id">Any integer.</param>
+        /// <param name="name">Any of the names in the data set.</param>
+        [TestCase(5, "Les Années Laser")]
+        [TestCase(9, "Sztereó Sound&Vision")]
+        [TestCase(int.MaxValue / 2, "qwertzuiop")]
+        [TestCase(int.MinValue, "Invalid")]
+        [TestCase(-1, null)]
+        [TestCase(0, "")]
+        public void TestGetOneMemberReturnsValidOrNull(int id, string name)
+        {
+            this.memberRepo.Setup(repo => repo.GetOne(id))
+                .Returns<int>(idArg => this.memberList.SingleOrDefault(m => m.MemberID == idArg));
+            this.memberRepo.Setup(repo => repo.GetOne(name))
+                .Returns<string>(nameArg => this.memberList.FirstOrDefault(m => m.Name == nameArg));
+
+            Member expectedMemberById = this.internalLogic.GetOneMember(id);
+            Member expectedMemberByName = this.internalLogic.GetOneMember(It.Is<int>(i => i < 1), name);
+
+            Assert.AreEqual(expectedMemberById, this.memberList.SingleOrDefault(m => m.MemberID == id));
+            Assert.AreEqual(expectedMemberByName, this.memberList.FirstOrDefault(m => m.Name == name));
+            Assert.AreEqual(expectedMemberById, expectedMemberByName);
+            this.memberRepo.Verify(repo => repo.GetOne(id), Times.AtMostOnce);
+            this.memberRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.AtMostOnce);
+            this.memberRepo.Verify(repo => repo.GetOne(name), Times.AtMostOnce);
+            this.memberRepo.Verify(repo => repo.GetOne(It.IsAny<string>()), Times.AtMostOnce);
+            this.memberRepo.Verify(repo => repo.GetAll(), Times.Never);
+        }
+
         /// <summary>
         /// Testing for the <see cref="InternalLogic"/> ListAll() methods.
         /// </summary>
         [Test]
         public void TestGetAll()
         {
-            Mock<ICountryRepository> mockedCountryRepo = new Mock<ICountryRepository>(MockBehavior.Loose);
-            Mock<IMemberRepository> mockedMemeberRepo = new Mock<IMemberRepository>(MockBehavior.Loose);
-            Mock<IExpertGroupRepository> mockedEGrepo = new Mock<IExpertGroupRepository>(MockBehavior.Loose);
-            List<Country> countries = MockRepoGenerator.GenerateCountryList();
-            List<Member> members = MockRepoGenerator.GenerateMemberList();
-            List<ExpertGroup> expertGroups = MockRepoGenerator.GenerateExpertGroupList();
-            mockedCountryRepo.Setup(repo => repo.GetAll()).Returns(countries.AsQueryable());
-            mockedEGrepo.Setup(repo => repo.GetAll()).Returns(expertGroups.AsQueryable());
-            mockedMemeberRepo.Setup(repo => repo.GetAll()).Returns(members.AsQueryable());
-            InternalAuditLogic internalAuditLogic = new InternalAuditLogic(2, mockedMemeberRepo.Object, mockedCountryRepo.Object, mockedEGrepo.Object);
-            List<ExpertGroup> sampleEGs = expertGroups;
-            Member firstMember = members[0];
+            this.countryRepo.Setup(repo => repo.GetAll()).Returns(this.countryList.AsQueryable);
+            this.memberRepo.Setup(repo => repo.GetAll()).Returns(this.memberList.AsQueryable);
+            this.egRepo.Setup(repo => repo.GetAll()).Returns(this.egList.AsQueryable);
+            Member firstMember = this.memberList[0];
+            Country lastCountry = this.countryList[^1];
 
-            IEnumerable<Country> allCountries = internalAuditLogic.ListAllCountries(out _);
-            IEnumerable<ExpertGroup> allEGs = internalAuditLogic.ListAllExpertgroups(out _);
-            IEnumerable<Member> allMembers = internalAuditLogic.ListAllMembers(out _);
+            IEnumerable<Country> allCountries = this.internalLogic.ListAllCountries(out int countAllCountries);
+            IEnumerable<ExpertGroup> allEGs = this.internalLogic.ListAllExpertgroups(out _);
+            IEnumerable<Member> allMembers = this.internalLogic.ListAllMembers(out _);
 
-            Assert.That(allCountries.Count(), Is.EqualTo(countries.Count));
-            Assert.That(expertGroups, Is.EquivalentTo(sampleEGs));
-            Assert.That(members, Does.Contain(firstMember));
-            mockedCountryRepo.Verify(repo => repo.GetAll(), Times.Once);
-            mockedEGrepo.Verify(repo => repo.GetAll(), Times.Once);
-            mockedMemeberRepo.Verify(repo => repo.GetAll(), Times.Once);
-            mockedCountryRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
-            mockedEGrepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
-            mockedMemeberRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
+            Assert.That(countAllCountries, Is.EqualTo(this.countryList.Count));
+            Assert.That(allCountries.ElementAt(countAllCountries - 1), Is.EqualTo(lastCountry));
+            Assert.That(allEGs, Is.EquivalentTo(this.egList));
+            Assert.That(allMembers.ElementAt(0), Is.EqualTo(firstMember));
+            this.countryRepo.Verify(repo => repo.GetAll(), Times.Once);
+            this.egRepo.Verify(repo => repo.GetAll(), Times.Once);
+            this.memberRepo.Verify(repo => repo.GetAll(), Times.Once);
+            this.countryRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
+            this.egRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
+            this.memberRepo.Verify(repo => repo.GetOne(It.IsAny<int>()), Times.Never);
         }
 
-        /*
         /// <summary>
-        /// [NOT WORKING] (NON-CRUD) Testing for <see cref="InternalAuditLogic.ListMembersInCapitalCity(bool)"/>.
+        /// (NON-CRUD) Testing for <see cref="InternalAuditLogic.ListMembersInCapitalCity(bool)"/>.
         /// </summary>
         [Test]
         public void TestListMembersInCapitalCity()
         {
-            Mock<ICountryRepository> mockedCountryRepo = new Mock<ICountryRepository>(MockBehavior.Loose);
-            Mock<IMemberRepository> mockedMemeberRepo = new Mock<IMemberRepository>(MockBehavior.Loose);
-            Mock<IExpertGroupRepository> mockedEGrepo = new Mock<IExpertGroupRepository>(MockBehavior.Loose);
-            List<Country> countries = MockRepoGenerator.GenerateCountryList();
-            List<Member> members = MockRepoGenerator.GenerateMemberList();
-            List<ExpertGroup> expertGroups = MockRepoGenerator.GenerateExpertGroupList();
-            mockedCountryRepo.Setup(repo => repo.GetAll()).Returns(countries.AsQueryable());
-            mockedEGrepo.Setup(repo => repo.GetAll()).Returns(expertGroups.AsQueryable());
-            mockedMemeberRepo.Setup(repo => repo.GetOne(It.IsAny<int>()));
-            mockedCountryRepo.Setup(repo => repo.GetOne(It.IsAny<int>()));
-            mockedEGrepo.Setup(repo => repo.GetOne(It.IsAny<int>()));
-            mockedMemeberRepo.Setup(repo => repo.GetAll()).Returns(members.AsQueryable());
-            InternalAuditLogic internalAuditLogic = new InternalAuditLogic(2, mockedMemeberRepo.Object, mockedCountryRepo.Object, mockedEGrepo.Object);
+            this.countryRepo.Setup(repo => repo.GetAll()).Returns(this.countryList.AsQueryable);
+            this.memberRepo.Setup(repo => repo.GetAll()).Returns(this.memberList.AsQueryable);
 
-            var results = internalAuditLogic.ListMembersInCapitalCity(true); // returns null bc string.contains??
+            IEnumerable<MemberCountry> membersInCaps = this.internalLogic.ListMembersInCapitalCity(true);
+            MemberCountry[] expectedIn = new MemberCountry[]
+            {
+                new() { Member = this.memberList[2], Country = this.countryList[2] },
+                new() { Member = this.memberList[3], Country = this.countryList[2] },
+                new() { Member = this.memberList[4], Country = this.countryList[4] },
+                new() { Member = this.memberList[8], Country = this.countryList[6] },
+                new() { Member = this.memberList[9], Country = this.countryList[6] },
+                new() { Member = this.memberList[10], Country = this.countryList[6] },
+                new() { Member = this.memberList[11], Country = this.countryList[7] },
+                new() { Member = this.memberList[12], Country = this.countryList[7] },
+                new() { Member = this.memberList[13], Country = this.countryList[8] },
+                new() { Member = this.memberList[14], Country = this.countryList[10] },
+            };
+            IEnumerable<MemberCountry> membersOutOfCaps = this.internalLogic.ListMembersInCapitalCity(false);
+            MemberCountry[] expectedOut = new MemberCountry[]
+            {
+                new() { Member = this.memberList[0], Country = this.countryList[0] },
+                new() { Member = this.memberList[1], Country = this.countryList[1] },
+                new() { Member = this.memberList[5], Country = this.countryList[5] },
+                new() { Member = this.memberList[6], Country = this.countryList[5] },
+                new() { Member = this.memberList[7], Country = this.countryList[5] },
+                new() { Member = this.memberList[15], Country = this.countryList[11] },
+                new() { Member = this.memberList[16], Country = this.countryList[8] },
+            };
 
-            mockedCountryRepo.Verify(repo => repo.GetAll(), Times.Once);
-            mockedMemeberRepo.Verify(repo => repo.GetAll(), Times.Once);
+            Assert.That(membersInCaps, Is.EquivalentTo(expectedIn));
+            Assert.That(membersOutOfCaps, Is.EquivalentTo(expectedOut));
+            this.countryRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
+            this.memberRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
         }
-    */
 
         /// <summary>
         /// (NON-CRUD) Testing for <see cref="InternalAuditLogic.GetRichestMemberInExpertGroup"/>.
@@ -83,45 +209,23 @@
         [Test]
         public void TestGetRichestMemberInExpertGroup()
         {
-            Mock<ICountryRepository> mockedCountryRepo = new Mock<ICountryRepository>(MockBehavior.Loose);
-            Mock<IMemberRepository> mockedMemeberRepo = new Mock<IMemberRepository>(MockBehavior.Loose);
-            Mock<IExpertGroupRepository> mockedEGrepo = new Mock<IExpertGroupRepository>(MockBehavior.Loose);
-            List<Country> countries = MockRepoGenerator.GenerateCountryList();
-            List<Member> members = MockRepoGenerator.GenerateMemberList();
-            List<ExpertGroup> expertGroups = MockRepoGenerator.GenerateExpertGroupList();
-            mockedCountryRepo.Setup(repo => repo.GetAll()).Returns(countries.AsQueryable());
-            mockedEGrepo.Setup(repo => repo.GetAll()).Returns(expertGroups.AsQueryable());
-            mockedMemeberRepo.Setup(repo => repo.GetAll()).Returns(members.AsQueryable());
-            InternalAuditLogic internalAuditLogic = new InternalAuditLogic(2, mockedMemeberRepo.Object, mockedCountryRepo.Object, mockedEGrepo.Object);
+            this.countryRepo.Setup(repo => repo.GetAll()).Returns(this.countryList.AsQueryable);
+            this.memberRepo.Setup(repo => repo.GetAll()).Returns(this.memberList.AsQueryable);
+            this.egRepo.Setup(repo => repo.GetAll()).Returns(this.egList.AsQueryable);
 
-            var results = internalAuditLogic.GetRichestMemberInExpertGroup();
-            var expectedResult = new List<ExpertgroupMemberCountry>
+            IEnumerable<ExpertgroupMemberCountry> results = this.internalLogic.GetRichestMemberInExpertGroup();
+            ExpertgroupMemberCountry[] expectedResult = new ExpertgroupMemberCountry[]
             {
-                new ExpertgroupMemberCountry
-                {
-                    Expertgroup = new ExpertGroup { ExpertGroupID = 1, Name = "Hi-Fi" },
-                    Member = new Member { MemberID = 8, Name = "Stereo", CountryID = 6, ExpertGroupID = 1 },
-                    Country = new Country { CountryID = 6, Name = "Germany", CapitalCity = "Berlin", CallingCode = 49, PPPperCapita = 53571 },
-                },
-                new ExpertgroupMemberCountry
-                {
-                    Expertgroup = new ExpertGroup { ExpertGroupID = 2, Name = "Home Theatre Audio" },
-                    Member = new Member { MemberID = 16, Name = "Sound & Vision", CountryID = 12, ExpertGroupID = 2, OfficeLocation = "Madison Avenue, 8 th floor, New York, NY 10016" },
-                    Country = new Country { CountryID = 12, Name = "USA", CapitalCity = "Washington, D.C.", CallingCode = 1, PPPperCapita = 63051 },
-                },
-                new ExpertgroupMemberCountry
-                {
-                    Expertgroup = new ExpertGroup { ExpertGroupID = 3, Name = "Home Theatre Display & Video" },
-                    Member = new Member { MemberID = 7, Name = "Hifi Test TV Video", CountryID = 6, ExpertGroupID = 3, OfficeLocation = "Gartroper Strasse 42, D-47138 - Duisburg", },
-                    Country = new Country { CountryID = 6, Name = "Germany", CapitalCity = "Berlin", CallingCode = 49, PPPperCapita = 53571 },
-                },
+                new() { Expertgroup = this.egList[0], Member = this.memberList[7], Country = this.countryList[5] },
+                new() { Expertgroup = this.egList[1], Member = this.memberList[15], Country = this.countryList[11] },
+                new() { Expertgroup = this.egList[2], Member = this.memberList[6], Country = this.countryList[5] },
                 /* no members in Photography group */
             };
 
             Assert.That(results, Is.EquivalentTo(expectedResult));
-            mockedCountryRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
-            mockedMemeberRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
-            mockedEGrepo.Verify(repo => repo.GetAll(), Times.Once);
+            this.countryRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
+            this.memberRepo.Verify(repo => repo.GetAll(), Times.Exactly(2));
+            this.egRepo.Verify(repo => repo.GetAll(), Times.Once);
         }
     }
 }
